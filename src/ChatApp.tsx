340ed0ab3e9a7d587517ab775655ab0276313b46
Noe Lopez Machado <noelopezm94@gmail.com>
2023-02-09 19:45:02 -0500

    Se configuro el el header, menu y seccion de chat

--- src/ChatApp.tsx
+++ src/ChatApp.tsx
@@ -1,7 +1,4 @@
-
-
-
-import React, {useState} from 'react';
+import React, {useRef, useState} from 'react';
 
 //theme
 import "primereact/resources/themes/lara-light-indigo/theme.css";
@@ -13,8 +10,14 @@ import "primereact/resources/primereact.min.css";
 import "primeicons/primeicons.css";
 
 import {Menu} from 'primereact/menu';
-import {InputText} from 'primereact/inputtext';
 import {Button} from 'primereact/button';
+import {Editor} from 'primereact/editor';
+import showdown from 'showdown';
+import {InputText} from "primereact/inputtext";
+import {MenuItem} from "primereact/menuitem";
+
+import './App.css';
+import {Divider} from "primereact/divider";
 
 interface Props {
 }
@@ -23,10 +26,10 @@ const ChatApp: React.FC<Props> = () => {
 
     const [items, setItems] = useState([
         {
-            label: 'General',
+            label: 'INFORMACIÓN',
             items: [
                 {
-                    label: 'All Team Members',
+                    label: 'General',
                     icon: 'pi pi-fw pi-users',
                     command: () => {
                         console.log('All Team Members channel selected');
@@ -42,18 +45,37 @@ const ChatApp: React.FC<Props> = () => {
             ]
         },
         {
-            label: 'Categories',
+            label: 'PROYECTOS',
             items: [
                 {
-                    label: 'Announcements',
-                    icon: 'pi pi-fw pi-bullhorn',
+                    label: 'Base de Datos',
+                    icon: 'pi pi-fw pi-database',
                     command: () => {
                         console.log('Announcements channel selected');
                     }
                 },
                 {
-                    label: 'Discussions',
+                    label: 'Discusiones',
                     icon: 'pi pi-fw pi-comments',
+                    command: () => {
+                        console.log('Discussions channel selected');
+                    }
+                }
+            ]
+        },
+        {
+            label: 'MENSAJES DIRECTOS',
+            items: [
+                {
+                    label: 'Alessandro Flores',
+                    icon: 'pi pi-fw pi-user',
+                    command: () => {
+                        console.log('Announcements channel selected');
+                    }
+                },
+                {
+                    label: 'Yandira Quispe',
+                    icon: 'pi pi-fw pi-user',
                     command: () => {
                         console.log('Discussions channel selected');
                     }
@@ -65,110 +87,123 @@ const ChatApp: React.FC<Props> = () => {
     const [chatHistory, setChatHistory] = useState<string[]>([]);
     const [message, setMessage] = useState<string>('');
 
+    const menu = useRef<Menu>(null);
+
+    let itemsMenuChannel: MenuItem[] = [
+        {label: 'Ver Información', icon: 'pi pi-fw pi-info'},
+        {separator: true},
+        {label: 'Mover a', icon: 'pi pi-fw pi-cog'},
+        {separator: true},
+        {label: 'Ver Miembros', icon: 'pi pi-fw pi-users'}
+    ]
+
     const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
         event.preventDefault();
-        setChatHistory([...chatHistory, message]);
+        const converter = new showdown.Converter();
+        const markdownMsg = converter.makeMarkdown(message);
+        setChatHistory([...chatHistory, markdownMsg]);
         setMessage('');
     };
 
-    const handleSendMessage = () => {
-        console.log(`Sending message: ${message}`);
-        setMessage('');
+    const renderHeader = () => {
+        return (
+            <>
+                <span className="ql-formats" style={{marginLeft: '0'}}>
+                    <button className="ql-bold"></button>
+                    <button className="ql-italic"></button>
+                    <button className="ql-strike"></button>
+                </span>
+                <span className="separator"></span>
+                <span className="ql-formats">
+                    <button className="ql-list" value="ordered"></button>
+                    <button className="ql-list" value="bullet"></button>
+                </span>
+                <span className="separator"></span>
+                <span className="ql-formats">
+                    <button className="ql-blockquote"></button>
+                    <button className="ql-code-block"></button>
+                </span>
+                <span className="separator"></span>
+                <span className="ql-formats">
+                    <button className="ql-link"></button>
+                </span>
+            </>
+        );
     };
 
     return (
-        <div style={{display: 'flex', height: '100vh'}}>
-            <div style={{width: '20%', height: '100%', backgroundColor: '#F7F7F7'}}>
+        <>
+            <header>
+                <span className="p-input-icon-left">
+                    <i className="pi pi-search"/>
+                    <InputText className="p-inputtext-sm" placeholder="Buscar en ESMA System" style={{width: '36rem'}}/>
+                </span>
+            </header>
+
+            <nav className="menu-nav">
                 <Menu model={items}/>
-            </div>
-            <div className="chat-section" style={{ width: '82%', height: '100%' }}>
-                <div className="chat-history" style={{ height: '85%', overflow: 'auto' }}>
+            </nav>
+
+            <main>
+
+                {/*Header chat*/}
+                <div>
+                    <div className="header-chat">
+                        <Button label="Base de Datos"
+                                icon="pi pi-angle-down"
+                                className="p-button-secondary p-button-text"
+                                style={{marginLeft: '5px'}}
+                                onClick={(e) => menu.current.toggle(e)}/>
+                        <Menu model={itemsMenuChannel} popup ref={menu}/>
+                    </div>
+
+
+                    <Divider style={{margin: '0'}}/>
+                </div>
+
+                {/*Historial de Chat*/}
+                <div>
                     {chatHistory.map((history, index) => (
                         <div key={index}>{history}</div>
                     ))}
                 </div>
-                <form onSubmit={handleSubmit} style={{ display: 'flex' }}>
-                    <InputText
-                        type="text"
-                        value={message}
-                        onChange={(e) => setMessage(e.target.value)}
-                        style={{ flex: '1' }}
-                    />
-                    <Button type="submit" label="Enviar" />
-                </form>
-            </div>
-        </div>
-    );
-};
 
-export default ChatApp;
+                {/*Input de Mensaje*/}
+                <div className="editor-footer">
+                    <div className="editor-message">
+                        <div style={{paddingBottom: '5px'}}>
+                            <small>Noe Lopez esta escribiendo ...</small>
+                        </div>
+                        <div>
+                            <form onSubmit={handleSubmit} onKeyDown={(event => {
+                                if (event.key === 'Enter') {
+                                    event.preventDefault();
+                                    handleSubmit(event)
+                                }
+                            })
+                            }>
+                                <div className="editor-custom">
+                                    <Editor
+                                        headerTemplate={renderHeader()}
+                                        value={message}
+                                        onTextChange={(e) => setMessage(e.htmlValue)}
+                                    />
 
+                                    <div className="editor-buttonbar">
+                                        <Button icon="pi pi-send" className="p-button-text" type="submit"
+                                                style={{padding: '0.2rem 1rem'}}/>
+                                    </div>
+                                </div>
 
-
-
-
-/*
-
-import React, { useState } from 'react';
-
-//theme
-import "primereact/resources/themes/lara-light-indigo/theme.css";
-
-//core
-import "primereact/resources/primereact.min.css";
-
-//icons
-import "primeicons/primeicons.css";
-
-import { Accordion, AccordionTab } from 'primereact/accordion';
-import { InputText } from 'primereact/inputtext';
-import { Button } from 'primereact/button';
-import './App.css';
-
-const ChatApp: React.FC = () => {
-
-    const [chatHistory, setChatHistory] = useState<string[]>([]);
-    const [message, setMessage] = useState<string>('');
-
-    const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
-        event.preventDefault();
-        setChatHistory([...chatHistory, message]);
-        setMessage('');
-    };
-
-    return (
-        <div className="container">
-            <div className="sidebar" style={{ width: '18%', height: '100%' }}>
-                <Accordion multiple={false}>
-                    <AccordionTab header="Categoría 1">
-                        Contenido de la categoría 1
-                    </AccordionTab>
-                    <AccordionTab header="Categoría 2">
-                        Contenido de la categoría 2
-                    </AccordionTab>
-                </Accordion>
-            </div>
-            <div className="chat-section" style={{ width: '82%', height: '100%' }}>
-                <div className="chat-history" style={{ height: '85%', overflow: 'auto' }}>
-                    {chatHistory.map((history, index) => (
-                        <div key={index}>{history}</div>
-                    ))}
+                            </form>
+                        </div>
+                    </div>
                 </div>
-                <form onSubmit={handleSubmit} style={{ display: 'flex' }}>
-                    <InputText
-                        type="text"
-                        value={message}
-                        onChange={(e) => setMessage(e.target.value)}
-                        style={{ flex: '1' }}
-                    />
-                    <Button type="submit" label="Enviar" />
-                </form>
-            </div>
-        </div>
+            </main>
+
+        </>
+
     );
 };
 
 export default ChatApp;
-
-
-*/
